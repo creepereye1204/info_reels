98fc4f0d4d96e1d00b33bb04b20538ac04148ee5
creepereye <creepereye1204@gmail.com>
2024-06-30 21:02:22 +0900

    2024-06-30:v2

--- info_reels_docker/apps/app/main.py
+++ info_reels_docker/apps/app/main.py
@@ -1,119 +1,26 @@
 # coding: utf-8
 import multiprocessing
-import os
-import random
-import time
-from datetime import date, datetime
 from typing import List
 
 import psutil
-import requests
 import uvicorn
-from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-from sqlalchemy import Column, Date, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
-load_dotenv()
-db_host = os.getenv('DB_URL')
+from .jobs import crawling
+from .models import InforDao
+from .schemas import InforSchema
+from .utils import get_db
+
 templates = Jinja2Templates(directory='info_reels_docker/apps/app/templates')
 app = FastAPI()
 app.mount('/static',
           StaticFiles(directory='info_reels_docker/apps/app/static'),
           name='static')
 pool = None
-engine = create_engine(db_host, connect_args={'options': '-c timezone=utc'})
-Base = declarative_base()
-
-
-class InforDao(Base):
-    __tablename__ = 'Info'
-    info_id = Column(Integer, primary_key=True, autoincrement=True)
-    notice = Column(Integer)
-    title = Column(String)
-    author = Column(String)
-    date = Column(Date)
-    view = Column(Integer)
-    link = Column(String)
-
-
-Base.metadata.create_all(engine)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.commit()
-        db.close()
-
-
-class InforSchema(BaseModel):
-    info_id: int
-    notice: int
-    title: str
-    author: str
-    date: date
-    view: int
-    link: str
-
-    class Config:
-        from_attributes = True
-
-
-def crawling():
-    MINUTE = 60
-    random_min = 10
-    random_max = 20
-    start_index = 1
-    end_index = 10
-    db = SessionLocal()
-    while True:
-        try:
-            db.query(InforDao).delete()
-            for index in range(start_index, end_index):
-                url = 'https://www.syu.ac.kr/academic/academic-notice/page/' \
-                      f'{index}/'
-                response = requests.get(url)
-                html = response.content.decode('utf-8')
-                soup = BeautifulSoup(html, 'html.parser')
-                notices = soup.findAll('th', class_='step1')
-                titles = soup.findAll('span', class_='tit')
-                authors = soup.findAll('td', class_='step3')
-                dates = soup.findAll('td', class_='step4')
-                views = soup.findAll('td', class_='step6')
-                links = soup.findAll('td', class_='step2')
-
-                for elements in zip(notices, titles, authors, dates, views,
-                                    links):
-                    notice, title, author, date, view, link = elements
-                    date_obj = datetime.strptime(
-                        date.text.strip(),
-                        '%Y.%m.%d').date()  # 날짜 문자열을 datetime.date 객체로 변환
-                    notice = notice.text.strip()
-                    infor = InforDao(
-                        notice=int(notice) if notice.isdigit() else 9999999,
-                        title=title.text.strip(),
-                        author=author.text.strip(),
-                        date=date_obj,  # 변환된 date 객체 사용
-                        view=int(view.text.strip().replace(',', '')),
-                        link=link.find('a', class_='itembx')['href'])
-                    db.add(infor)
-        except Exception as e:
-            print(f'Error occurred: {e}')
-            db.rollback()
-        finally:
-            db.commit()
-            db.close()
-        time.sleep(MINUTE * random.randint(random_min, random_max))
 
 
 @app.get('/items', response_model=List[InforSchema])
@@ -131,10 +38,9 @@ async def get_settings(request: Request):
 
 @app.get('/dashboard')
 def get_usage_data():
-    cpu_usage = round(psutil.cpu_percent(interval=1), 2)
-    hdd_usage = round(psutil.disk_usage('/').percent, 2)
-    mem_usage = round(psutil.virtual_memory().percent, 2)
-
+    cpu_usage = int(psutil.cpu_percent(interval=1))
+    hdd_usage = int(psutil.disk_usage('/').percent)
+    mem_usage = int(psutil.virtual_memory().percent)
     return {
         'cpu_usage': cpu_usage,
         'hdd_usage': hdd_usage,
